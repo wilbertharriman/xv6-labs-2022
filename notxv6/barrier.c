e4d3f006032e05abf2a7552053d115038204a6ac
Wilbert Harriman <wilbertharriman1@gmail.com>
2024-04-18 22:01:49 +0800

    barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,31 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  static int all_exited = 1;
+
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  while (!all_exited)
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+
+  bstate.nthread++;
+
+  if (bstate.nthread < nthread) {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  } else {
+    all_exited = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+
+  bstate.nthread--;
+
+  if (bstate.nthread == 0) {
+    all_exited = 1;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
